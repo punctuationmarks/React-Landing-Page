617551f66e058e01c2c3e2b1bb4d3df6a0bb71c6
punctuationmarks <punctuationmarks@protonmail.com>
2020-05-06 17:42:03 -0400

    updated images

--- src/components/Portfolio/ModalComponent.js
+++ src/components/Portfolio/ModalComponent.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Modal, Button, Image } from 'react-bootstrap';
-
+import { Modal, Button } from 'react-bootstrap';
+import { LazyLoadImage } from "react-lazy-load-image-component"
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 function ModalComponent(props) {
   return (
@@ -17,19 +18,19 @@ function ModalComponent(props) {
       <Modal.Body>
         <h4 className="modal-subtitle">{props.subtitle_1}</h4>
         <p>{props.body_1}</p>
-        <Image src={props.image_a} fluid className="p-2 modal-image" />
-        <Image src={props.image_b} fluid className="p-2 modal-image" />
-        <Image src={props.image_c} fluid className="p-2 modal-image" />
+
+        <LazyLoadImage effect="blur" src={props.image_a} className="modal-image" />
+        <LazyLoadImage effect="blur" src={props.image_b} className="modal-image" />
+        <LazyLoadImage effect="blur" src={props.image_c} fluid className="modal-image" />
 
         <h4 className="modal-subtitle">{props.subtitle_2}</h4>
         <p>{props.body_2}</p>
 
-        <Image src={props.image_d} fluid className="p-2 modal-image" />
-        <Image src={props.image_e} fluid className="p-2 modal-image" />
-        <Image src={props.image_f} fluid className="p-2 modal-image" />
+        <LazyLoadImage effect="blur" src={props.image_d} fluid className="modal-image" />
+        <LazyLoadImage effect="blur" src={props.image_e} fluid className="modal-image" />
+        <LazyLoadImage effect="blur" src={props.image_f} fluid className="modal-image" />
+        <LazyLoadImage effect="blur" src={props.image_g} fluid className="modal-image" />
 
-        <Image src={props.image_g} fluid className="p-2 modal-image" />
-        <Image src={props.image_h} fluid className="p-2 modal-image" />
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
